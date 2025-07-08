b9b8fda01b64631dab3781bde916229dd1063f30
Vinicius <vini_532cursos@hotmail.com>
2025-07-07 21:33:49 -0300

    chore: initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ConceitosManualModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
