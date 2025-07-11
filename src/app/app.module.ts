8fdab6b45e0693c88a788d2ddab60f179692a946
Vinicius <vini_532cursos@hotmail.com>
2025-07-11 18:53:38 -0300

    feat: Implementando conceitos básicos sobre modules controllers e providers.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
 import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConceitosManualModule],
+  imports: [ConceitosManualModule, ConceitosAutomaticoModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
-
+export class AppModule { }
