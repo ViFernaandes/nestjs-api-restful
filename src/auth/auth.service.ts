a04aafe28330cba7067a5880b11841b71d1c2204
Vinicius <vini_532cursos@hotmail.com>
2025-08-15 13:49:23 -0300

    feat: Implementado sistema de JWT.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
+import { Repository } from 'typeorm';
+import { HashingServiceProtocol } from './hashing/hashing.service';
+import jwtConfig from './config/jwt.config';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService { }
+export class AuthService {
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+    private readonly hashingService: HashingServiceProtocol,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private readonly jwtService: JwtService,
+  ) { }
+
+  async login(loginDto: LoginDto) {
+    let passwordIsValid = false;
+    let throwError = true;
+
+    const pessoa = await this.pessoaRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    if (pessoa) {
+      passwordIsValid = await this.hashingService.compare(
+        loginDto.password,
+        pessoa.passwordHash,
+      );
+    }
+
+    if (passwordIsValid) {
+      throwError = false;
+    }
+
+    if (throwError) {
+      throw new UnauthorizedException('Usuário ou senha inválidos.');
+    }
+
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: pessoa.id,
+        email: pessoa.email,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.tll,
+      },
+    );
+
+    return {
+      accessToken,
+    };
+  }
+}
